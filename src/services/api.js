d77cb1a0b95fb4013d0a2400371a0030194be092
Elena Valero <elenavaleroesco@gmail.com>
2024-02-14 19:24:07 +0100

    HTML file

---
+++ src/services/api.js
@@ -0,0 +1,23 @@
+const apiData = () => {
+    return fetch('https://hp-api.onrender.com/api/characters/')
+      .then((response) => response.json())
+      .then((data) => {
+        const dataChar = data.map((character) => {
+          const characterData = {
+            id: character.id,
+            name: character.name,
+            alternateNames: character.alternate_names,
+            specie: character.species,
+            gender: character.gender,
+            house: character.house,
+            alive: character.alive,
+            image: character.image,
+            wizard: character.wizard,
+          };
+          return characterData;
+        });
+        return dataChar;
+      });
+  };
+  
+  export default apiData;
